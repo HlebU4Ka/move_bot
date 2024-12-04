9d3e02f4bb73a4f5f5ea1e0e82dc6b2e0da37ea3
Gleb <glebkot57@gmail.com>
2024-12-04 23:12:02 +0200

    V2 Поднялось все кроме бота.

---
+++ init_db.py
@@ -0,0 +1,10 @@
+from database.database import  engine
+from database.models.models import Base
+
+def init_database():
+    Base.metadate.create_all(bind=engine)
+
+    if __name__ == "__main__":
+        print("Инициализация базы данных...")
+        init_database()
+        print("База данных успешно инициализирована!")
